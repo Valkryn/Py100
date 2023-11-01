fe63af6c7a359034a406dedcbb7131bf6f95d4f4
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-10-31 20:20:01 -0400

    day1

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+# create a name band generator from user input
+
+def main():
+    city = input('What city did you grow up in? \n')
+    pet = input('What is the name of your pet? \n')
+    print(f'Your band name is : \n {city} {pet}')
+
+
+if __name__ == '__main__':
+    main()
